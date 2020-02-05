12e0b744e7f16d3874f3f03710d2447af5aa8d53
Irtaza Nadeem <irtaza.nadeem542@gmail.com>
2020-02-06 00:15:41 +0500

    updated

--- src/editPost.jsx
+++ src/editPost.jsx
@@ -9,34 +9,51 @@ class EditPostComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: []
         }
     }
 
     onChange = (e) => {
-             //this.props.post.title: e.target.value
+
     };
 
     onSubmit = (e) => {
-
+        const data = this.props.post;
+        this.props.editUserProp(data, this.props.match.params.id);
     };
 
-
     componentDidMount() {
-
+        this.props.getPost(this.props.match.params.id);
     }
 
     render() {
-
+        const {title, body} = this.props.post;
+        return <div>
+            <h1>Edit Post</h1>
+            <h3>{title}</h3>
+            <div>
+                <span>Title</span>
+                <input type="text" name="title" defaultValue={title} onChange={this.onChange}/>
+            </div>
+            <div>
+                <span>Body</span>
+                <input
+                    type="text"
+                       name="body"
+                    defaultValue={body}
+                       onChange={this.onChange} />
+            </div>
+            <Link  to={"/posts/"+this.props.match.params.id}><button onClick={this.onSubmit}>Update</button></Link>
+        </div>
     }
 }
 
 const mapStateToProps = (state) => ({
-
+    post: state.posts,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+    getPost: (id) => getPost(dispatch, id),
+    editUserProp: (data, id) => editPost(dispatch, data, id)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditPostComponent);
